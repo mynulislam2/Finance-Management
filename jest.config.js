a67177f7c13f3d4945538cd0bcd34b694362fc1c
Mynu Islam <68342123+mynulislam2@users.noreply.github.com>
2026-07-01 02:05:28 +0600

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: '@react-native/jest-preset',
   setupFiles: ['./jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|react-redux|@reduxjs/toolkit|immer|@react-navigation|victory-native|@supabase|date-fns|react-native-vector-icons)/)',
+    'node_modules/(?!(react-native|react-native-config|@react-native|react-redux|@reduxjs/toolkit|immer|@react-navigation|victory-native|@supabase|date-fns|react-native-vector-icons)/)',
   ],
   moduleNameMapper: {
     '@react-native-community/datetimepicker': '<rootDir>/__mocks__/empty.js',
